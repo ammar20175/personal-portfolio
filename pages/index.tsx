70c501a794386b892732a724cdb06f6d01f93b68
ammarahmad <ammar.ahmad6410@gmail.com>
2023-12-02 11:04:14 +0400

    setup plus inital header component done.

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import Header from "@/components/Header";
+import Head from "next/head";
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Ammar's Portfolio</title>
+			</Head>
+
+			{/* header */}
+			<Header />
+			{/* hero */}
+
+			{/* about */}
+
+			{/* experience */}
+
+			{/* skills */}
+
+			{/* porjects */}
+
+			{/* contact me */}
+		</div>
+	);
+}
