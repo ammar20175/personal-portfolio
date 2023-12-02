21d0ceccfd19be08608a2eda96a9f87ec8b7259f
ammarahmad <ammar.ahmad6410@gmail.com>
2023-12-02 13:14:47 +0400

    hero ui done.

---
+++ components/Hero.tsx
@@ -0,0 +1,56 @@
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import Link from "next/link";
+
+type Props = {};
+
+export default function Hero({}: Props) {
+	const [text, count] = useTypewriter({
+		words: [
+			"Hi,My Name is Ammar Ahmad",
+			"Guy-who-loves-coffee.tsx",
+			"<ButLovesToCodeMore />",
+		],
+		loop: true,
+		delaySpeed: 2000,
+	});
+
+	return (
+		<div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+			<BackgroundCircles />
+			<img
+				className="relative rounded-full h-32 w-32 mx-auto object-cover"
+				src="https://images.unsplash.com/photo-1588862081167-d5b98006637e?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+			/>
+
+			<div className="z-20">
+				<h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+					Computer Systems Engineer
+				</h2>
+
+				<h1 className="text-5xl lg:text-6xl font-semibold px-10">
+					<span className="mr-3">{text}</span>
+					<Cursor cursorColor="#F7AB0A" />
+				</h1>
+
+				<div className="pt-5">
+					<Link href="#about">
+						<button className="heroButton">About</button>
+					</Link>
+
+					<Link href="#experience">
+						<button className="heroButton">Experience</button>
+					</Link>
+
+					<Link href="#skills">
+						<button className="heroButton">Skills</button>
+					</Link>
+
+					<Link href="#projects">
+						<button className="heroButton">Projects</button>
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import Head from "next/head";
 
 export default function Home() {
 	return (
-		<div>
+		<div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
 			<Head>
 				<title>Ammar's Portfolio</title>
 			</Head>
 
-			{/* header */}
 			<Header />
-			{/* hero */}
+
+			<section id="hero" className="snap-center">
+				<Hero />
+			</section>
 
 			{/* about */}
 
